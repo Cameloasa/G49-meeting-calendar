c5df1b2e0406536d80b4042254b8f8a910a434ab
Camelia Ciuca <camelia1414@gmail.com>
2024-05-16 11:36:24 +0200

    MeetingDaoImpl changes

---
+++ src/main/java/se/lexicon/dao/CalendarDao.java
@@ -0,0 +1,21 @@
+package se.lexicon.dao;
+
+import se.lexicon.model.Calendar;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface CalendarDao {
+
+    Calendar createCalendar(String username, String title);
+
+    Optional<Calendar> findById(int id);
+
+    Optional<Calendar> findByTitle(String title);
+
+    Collection<Calendar> findCalendarsByUsername(String username);
+
+    boolean deleteCalendar(int id);
+
+
+}
---
+++ src/main/java/se/lexicon/dao/UserDao.java
@@ -0,0 +1,16 @@
+package se.lexicon.dao;
+
+import se.lexicon.exceptions.AuthenticationFieldsException;
+import se.lexicon.exceptions.UserExpiredException;
+import se.lexicon.model.User;
+
+import java.util.Optional;
+
+public interface UserDao {
+
+     User createUser(String username);
+     Optional<User> findByUsername(String username);
+     boolean authenticate(User user) throws UserExpiredException, AuthenticationFieldsException;
+
+     //Add more methods
+}
